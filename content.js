e8236e4ad84d87a6e541a984dc55ab49e66cb0f9
katarina <kavucic@icloud.com>
2024-02-18 00:17:03 -0500

    added esg (kinda)

--- content.js
+++ content.js
@@ -15,6 +15,9 @@
     function hi() {
         const imgLink = chrome.runtime.getURL('worthit-popup.png');
         const imgLink1 = chrome.runtime.getURL('stanley.jpg');
+        const logo = chrome.runtime.getURL('logo.svg');
+        const exit = chrome.runtime.getURL('exit.svg');
+        const settings = chrome.runtime.getURL('settings.svg');
         const brand = "Stanley"
         const title = "Stanley Quencher H2.O FlowState™ Tumbler 30oz Rose Quartz"
         const price = "CAD $39.29"
@@ -23,7 +26,16 @@
 
         const home = ` <img class="sidePopImage" src="${imgLink}" alt="plus sign">
         <div class="mainFrame">
-            <div class="nav-bar"></div>
+            <div class="nav-bar">
+                <div class="left-svg">
+                    <img src="${logo}" alt="WorthIt Logo">
+                </div>
+            
+                <div class="right-svgs">
+                    <img src="${settings}" alt="Right SVG 1">
+                    <img src="${exit}" alt="Right SVG 2">
+                </div>
+            </div>
 
             <div class="product-detail">
                 <div class="product-image"><img class="main-img" src="${imgLink1}" alt="plus sign"></div>
@@ -34,7 +46,10 @@
                 <div class="tab-el"> <p class="p-tab">ESG Score</p> </div>
                 <div class="tab-el"> <p class="p-tab">Worth It?</p></div>
  </div>
- <div class= "tabContent"> <p>This is the Overview tab content.</p></div>
+ <div class= "tabContent"> 
+    <p>This is the Overview tab content.</p>
+    
+ </div>
  </div></div></div>`;
 
         const overview = `<div class="pros-section> <div/>"`
@@ -53,6 +68,10 @@
 
 function addTabContent(tabName) {
     // Remove any previously added tab content
+    const esg_circle = chrome.runtime.getURL('esg_circle.svg');
+    const worthit_circle = chrome.runtime.getURL('worthit_meter.svg');
+    const esg_bars = chrome.runtime.getURL('esg_bars.svg');
+    const esg_explanation = chrome.runtime.getURL('esg_explanation.svg');   
     var tabContent = document?.getElementsByClassName("tabContent")[0];
     if (tabContent) {
         tabContent.parentNode.removeChild(tabContent);
@@ -67,7 +86,26 @@ function addTabContent(tabName) {
     } else if (tabName === "Worth It?") {
         newContent.innerHTML = `<p>This is the Worth It? tab content.</p>`;
     } else {
-        newContent.innerHTML = `<p>This is the ESG Score tab content.</p>`;
+        newContent.innerHTML = `
+        <div class="esg_width">
+            <div class="esg">
+                <img src="${esg_circle}" alt="ESG Circle">
+                <div class="text_overlay">25</div>
+            </div>
+            <p>The final ESG Risk Scores are a measure of unmanaged risk on an absolute scale of 0-100, with a lower score signaling less unmanaged ESG Risk.</p>
+            <div class="esg"> 
+                <img src="${esg_bars}" alt="ESG Bars">
+            </div>
+            <p class="bold"> What is ESG?</p>
+            <p>An Environmental, Social and Governance (ESG) score measures an organization’s performance against various sustainability metrics related to environmental, social, or governance issues.</p>
+            <div class="esg"> 
+                <img src="${esg_explanation}" alt="ESG Explanation">
+            </div>
+            <p>ESG scores are determined by analysts who evaluate corporate disclosures, conduct management interviews, and review public information to rate an organization objectively.</p>
+            <p class="bold">Good to know</p>
+            <p>A low ESG score indicates effective management of environmental, social, and governance risks, reflecting commitment to sustainability. Conversely, a high ESG score implies greater risk exposure in these areas.</p>
+        </div>
+        `;
     }
 
     // Append new tab content to the container
